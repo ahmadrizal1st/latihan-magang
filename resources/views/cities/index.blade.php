3d4d9408e44ea3687e7214ae7fa5f4219a33f205
Ahmad Rizal <ahmaddrizalul@gmail.com>
2026-02-26 15:29:30 +0700

    feat: Add modals for creating, editing, and deleting City, Employee Job, and Employee records with corresponding AJAX functionality

--- resources/views/cities/index.blade.php
+++ resources/views/cities/index.blade.php
@@ -15,7 +15,12 @@
             <div class="box">
 
                 <div class="box-header">
-                    <h3 class="box-title">city Data Table</h3>
+                    <h3 class="box-title">City Data Table</h3>
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modalCreateCity">
+                            <i class="fa fa-plus"></i> Add City
+                        </button>
+                    </div>
                 </div>
                 <div class="box-body">
                     <table id="example1" class="table table-bordered table-striped">
@@ -24,6 +29,7 @@
                                 <th>No</th>
                                 <th>Name</th>
                                 <th>Employee</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody id="city-table-body">
@@ -34,6 +40,7 @@
                                 <th>No</th>
                                 <th>Name</th>
                                 <th>Employee</th>
+                                <th>Action</th>
                             </tr>
                         </tfoot>
                     </table>
@@ -47,12 +54,111 @@
     </div>
     {{-- /.row --}}
 </section>
+
+{{-- Modal Create City --}}
+<div class="modal fade" id="modalCreateCity" tabindex="-1" role="dialog" aria-labelledby="modalCreateCityLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="modalCreateCityLabel">
+                    <i class="fa fa-plus-circle"></i> Create City
+                </h4>
+            </div>
+            <form id="formCreateCity" role="form">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="cityName">City Name <span class="text-red">*</span></label>
+                        <input type="text" class="form-control" id="cityName" name="name" placeholder="Enter city name...">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">
+                        <i class="fa fa-times"></i> Cancel
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fa fa-save"></i> Save
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+{{-- /.modal --}}
+
+{{-- Modal Edit City --}}
+<div class="modal fade" id="modalEditCity" tabindex="-1" role="dialog" aria-labelledby="modalEditCityLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="modalEditCityLabel">
+                    <i class="fa fa-pencil"></i> Edit City
+                </h4>
+            </div>
+            <form id="formEditCity" role="form">
+                <input type="hidden" id="editCityId" name="id">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="editCityName">City Name <span class="text-red">*</span></label>
+                        <input type="text" class="form-control" id="editCityName" name="name" placeholder="Enter city name...">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">
+                        <i class="fa fa-times"></i> Cancel
+                    </button>
+                    <button type="submit" class="btn btn-warning">
+                        <i class="fa fa-save"></i> Update
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+{{-- /.modal --}}
+
+{{-- Modal Confirm Delete --}}
+<div class="modal fade" id="modalDeleteCity" tabindex="-1" role="dialog" aria-labelledby="modalDeleteCityLabel">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #dd4b39; color: #fff;">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" style="color:#fff;">&times;</span>
+                </button>
+                <h4 class="modal-title" id="modalDeleteCityLabel">
+                    <i class="fa fa-trash"></i> Confirm Delete
+                </h4>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to delete <strong id="deleteCityName"></strong>?</p>
+                <p class="text-muted"><small>This action cannot be undone.</small></p>
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" id="deleteCityId">
+                <button type="button" class="btn btn-default" data-dismiss="modal">
+                    <i class="fa fa-times"></i> Cancel
+                </button>
+                <button type="button" class="btn btn-danger" id="btnConfirmDelete">
+                    <i class="fa fa-trash"></i> Delete
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- /.modal --}}
+
 @endsection
 
 @pushOnce('scripts')
 <script>
 $(function () {
-    $('#example1').DataTable({
+    // Initialize DataTable
+    var table = $('#example1').DataTable({
         processing: true,
         serverSide: true,
         ajax: {
@@ -72,14 +178,150 @@ $(function () {
             },
             {
                 data: 'employees',
-                orderable: false, 
+                orderable: false,
                 searchable: false,
                 render: function(data) {
                     return data.length;
                 }
+            },
+            {
+                data: 'id',
+                orderable: false,
+                searchable: false,
+                render: function(data, type, row) {
+                    return `
+                        <button class="btn btn-warning btn-xs btn-edit"
+                            data-id="${data}"
+                            data-name="${row.name}"
+                            data-description="${row.description ?? ''}">
+                            <i class="fa fa-pencil"></i> Edit
+                        </button>
+                        <button class="btn btn-danger btn-xs btn-delete"
+                            data-id="${data}"
+                            data-name="${row.name}">
+                            <i class="fa fa-trash"></i> Delete
+                        </button>
+                    `;
+                }
             }
         ],
     });
+
+    // CREATE
+    $('#formCreateCity').on('submit', function(e) {
+        e.preventDefault();
+
+        var $btn = $(this).find('[type="submit"]');
+        $btn.prop('disabled', true).html('<i class="fa fa-spinner fa-spin"></i> Saving...');
+
+        $.ajax({
+            url: '/api/city',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function(response) {
+                $('#modalCreateCity').modal('hide');
+                table.ajax.reload();
+            },
+            error: function(xhr) {
+                handleValidationErrors(xhr, '#formCreateCity');
+            },
+            complete: function() {
+                $btn.prop('disabled', false).html('<i class="fa fa-save"></i> Save');
+            }
+        });
+    });
+
+    // OPEN EDIT MODAL
+    $('#example1').on('click', '.btn-edit', function() {
+        var id          = $(this).data('id');
+        var name        = $(this).data('name');
+        var description = $(this).data('description');
+
+        $('#editCityId').val(id);
+        $('#editCityName').val(name);
+
+        $('#modalEditCity').modal('show');
+    });
+
+    // EDIT SUBMIT
+    $('#formEditCity').on('submit', function(e) {
+        e.preventDefault();
+
+        var id   = $('#editCityId').val();
+        var $btn = $(this).find('[type="submit"]');
+        $btn.prop('disabled', true).html('<i class="fa fa-spinner fa-spin"></i> Updating...');
+
+        $.ajax({
+            url: '/api/city/' + id,
+            type: 'PUT',
+            data: $(this).serialize(),
+            success: function(response) {
+                $('#modalEditCity').modal('hide');
+                table.ajax.reload(null, false);
+            },
+            error: function(xhr) {
+                handleValidationErrors(xhr, '#formEditCity');
+            },
+            complete: function() {
+                $btn.prop('disabled', false).html('<i class="fa fa-save"></i> Update');
+            }
+        });
+    });
+
+    // OPEN DELETE MODAL
+    $('#example1').on('click', '.btn-delete', function() {
+        var id   = $(this).data('id');
+        var name = $(this).data('name');
+
+        $('#deleteCityId').val(id);
+        $('#deleteCityName').text(name);
+
+        $('#modalDeleteCity').modal('show');
+    });
+
+    // CONFIRM DELETE
+    $('#btnConfirmDelete').on('click', function() {
+        var id   = $('#deleteCityId').val();
+        var $btn = $(this);
+        $btn.prop('disabled', true).html('<i class="fa fa-spinner fa-spin"></i> Deleting...');
+
+        $.ajax({
+            url: '/api/city/' + id,
+            type: 'DELETE',
+            success: function(response) {
+                $('#modalDeleteCity').modal('hide');
+                table.ajax.reload(null, false);
+            },
+            error: function(xhr) {
+                alert('Failed to delete. Please try again.');
+            },
+            complete: function() {
+                $btn.prop('disabled', false).html('<i class="fa fa-trash"></i> Delete');
+            }
+        });
+    });
+
+    // RESET MODALS ON CLOSE
+    $('#modalCreateCity, #modalEditCity').on('hidden.bs.modal', function() {
+        $(this).find('form')[0].reset();
+        $(this).find('.form-group').removeClass('has-error');
+        $(this).find('.help-block.error-msg').remove();
+    });
+
+    // HELPER: Validation Errors
+    function handleValidationErrors(xhr, formSelector) {
+        $(formSelector + ' .form-group').removeClass('has-error');
+        $(formSelector + ' .help-block.error-msg').remove();
+
+        var errors = xhr.responseJSON?.errors;
+        if (errors) {
+            $.each(errors, function(field, messages) {
+                var $input = $(formSelector + ' [name="' + field + '"]');
+                $input.closest('.form-group').addClass('has-error');
+                $input.after('<span class="help-block error-msg">' + messages[0] + '</span>');
+            });
+        }
+    }
 });
 </script>
 @endPushOnce
