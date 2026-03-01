c0b675a7edf73f954e7a6cc42d819e12fad42f6d
Ahmad Rizal <ahmaddrizalul@gmail.com>
2026-03-01 14:08:23 +0700

    refactor: model province, city, district, counter

--- app/Models/District.php
+++ app/Models/District.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class District extends Model
-{
+{    
     protected $fillable = ['name', 'city_id'];
 
     /**
--- app/Models/Province.php
+++ app/Models/Province.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Staudenmeir\EloquentHasManyDeep\HasRelationships;
 
 class Province extends Model
 {
+    use HasRelationships;
+    
     protected $fillable = ['name'];
 
     /**
